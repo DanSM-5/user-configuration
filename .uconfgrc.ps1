aff93ec8fed0592be49065007d563a13144f48b8
Daniel <eduardo.daniel.s.m@gmail.com>
2022-03-28 21:34:36 -0400

    feat: Update general configuration

--- .uconfgrc.ps1
+++ .uconfgrc.ps1
@@ -5,13 +5,39 @@
 
 # Follow structure conf folders and files
 $user_conf_path = "$HOME\.usr_conf\.uconfrc.ps1"
-$user_scripts_path = "C:/user-scripts"
-$prj = "$HOME/prj"
+$user_scripts_path = "C:\user-scripts"
+$prj = "$HOME\prj"
+
+function testCommand {
+  Param ($command)
+  $oldPreference = $ErrorActionPreference
+  $ErrorActionPreference = 'stop'
+  try {
+    if (Get-Command $command) { return $true }
+  } catch { return $false }
+  finally { $ErrorActionPreference = $oldPreference }
+}
+
+if ((testCommand oh-my-posh) -and (Test-Path "${HOME}\omp-theme")) {
+  # Import-Module oh-my-posh
+  $env:POSH_GIT_ENABLED = $true
+  $env:POSH_THEMES_PATH = "${HOME}\omp-theme"
+
+  oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/jandedobbeleer.omp.json | Invoke-Expression
+}
+
+if (testCommand Set-PsFzfOption) {
+  # fzf
+  # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+  Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+  Set-PSFzfOption -EnableAliasFuzzyEdit -EnableAliasFuzzyFasd -EnableAliasFuzzyHistory -EnableAliasFuzzyKillProcess -EnableAliasFuzzySetLocation -EnableAliasFuzzySetEverything -EnableAliasFuzzyZLocation -EnableAliasFuzzyGitStatus
+  Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+}
 
 # Set Emacs keybindings for readline
 # Set-PSReadLineOption -EditMode Emacs
 
-# Set Prediction
+# Set Prediction - PS 7.1 or above only
 if ($PSVersionTable.PSVersion -ge 7.1) {
   Set-PSReadLineOption -PredictionSource History
   Set-PSReadLineOption -Colors @{ InlinePrediction = "#B3E5FF" }
