0277402fd29dd99887eaa9b16fe5bc9a4c2272e9
Daniel <eduardo.daniel.s.m@gmail.com>
2022-05-17 11:22:55 -0400

    feat: Add bat to some fzf-git utils

--- utils/fzf-git.sh
+++ utils/fzf-git.sh
@@ -16,8 +16,9 @@ fgf() {
   is_in_git_repo || return
   git -c color.status=always status --short |
   fzf-down -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always -- {-1} | sed 1,4d; bat -pp --color=always {-1})' |
+    --preview '(git diff --color=always -- {-1} | sed 1,4d | bat -p --color=always; bat --color=always {-1})' |
   cut -c4- | sed 's/.* -> //'
+  # --preview '(git diff --color=always -- {-1} | sed 1,4d | bat -p --color=always; cat {-1})' |
 }
 
 fgb() {
@@ -33,7 +34,8 @@ fgt() {
   is_in_git_repo || return
   git tag --sort -version:refname |
   fzf-down --multi --preview-window right:70% \
-    --preview 'git show --color=always {}'
+    --preview 'git show --color=always {} | bat --color=always'
+    # --preview 'git show --color=always {}'
 }
 
 fgh() {
@@ -41,7 +43,17 @@ fgh() {
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | bat -p --color=always' |
+  grep -o "[a-f0-9]\{7,\}"
+  # --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
+}
+
+fgha() {
+  is_in_git_repo || return
+  git log --all --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | bat -p --color=always' |
   grep -o "[a-f0-9]\{7,\}"
 }
 
@@ -55,6 +67,7 @@ fgr() {
 
 fgs() {
   is_in_git_repo || return
-  git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
+  git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1} | bat -p --color=always' |
   cut -d: -f1
+  # git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
 }
