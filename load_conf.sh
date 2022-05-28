3451e3c94b4272b0d7448a1b2f5ec5c43a718c00
Daniel <eduardo.daniel.s.m@gmail.com>
2022-05-28 11:21:01 -0400

    feat: Add system detection variables

--- load_conf.sh
+++ load_conf.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Default values for device detection
+export IS_WSL=false
+export IS_LINUX=false
+export IS_MAC=false
+
 # Detect if running WSL
-# if command -v /mnt/c/Windows/System32/cmd.exe &> /dev/null; then
-#   export IS_WSL=true 
-# fi
+if command -v /mnt/c/Windows/System32/cmd.exe &> /dev/null; then
+  export IS_WSL=true 
+fi
 
 # Source User Scripts
 test -f "$HOME/.usr_conf/.uconfgrc" && \. "$HOME/.usr_conf/.uconfgrc"
