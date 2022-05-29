40a9ccf9f07912d27b9930a2d4da3b89599234f6
Daniel <eduardo.daniel.s.m@gmail.com>
2022-05-28 23:17:33 -0400

    feat: Add termux detection

--- load_conf.sh
+++ load_conf.sh
@@ -2,12 +2,15 @@
 
 # Default values for device detection
 export IS_WSL=false
+export IS_TERMUX=false
 export IS_LINUX=false
 export IS_MAC=false
 
 # Detect if running WSL
 if command -v /mnt/c/Windows/System32/cmd.exe &> /dev/null; then
   export IS_WSL=true 
+elif command -v termux-setup-storage &> /dev/null; then
+  export IS_TERMUX=true
 fi
 
 # Source User Scripts
