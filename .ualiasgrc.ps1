a35778212f8cbb0ff1a0b99a2eaac40afd613289
Daniel <eduardo.daniel.s.m@gmail.com>
2022-06-05 16:54:16 -0400

    feat: Add proxy function for With-Env script

--- .ualiasgrc.ps1
+++ .ualiasgrc.ps1
@@ -159,6 +159,11 @@ function ... {
   cd ../..
 }
 
+function info () {
+  # can also be piped into less.exe
+  Get-Help -Full $args[0] | bat -l man -p
+}
+
 function up ([int] $val = 1) {
   $cmmd = ""
   if ($val -le 0) {
@@ -262,3 +267,49 @@ function ex ([String] $filename) {
     echo "'$filename' is not a file"
   }
 }
+
+# Mimic env.exe to include env variables to call a ps1 script
+# E.g. With-Env VAR=VAL script.ps1
+# Ref: https://devblogs.microsoft.com/scripting/proxy-functions-spice-up-your-powershell-core-cmdlets/
+# PS > $MetaData = New-Object System.Management.Automation.CommandMetaData (Get-Command  Some-Command)
+# PS > [System.Management.Automation.ProxyCommand]::Create($MetaData)
+function With-Env () {
+  param()
+
+  begin
+  {
+      try {
+          $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand("$user_conf_path\utils\With-Env.ps1", [System.Management.Automation.CommandTypes]::ExternalScript)
+          $PSBoundParameters.Add('$args', $args)
+          $scriptCmd = {& $wrappedCmd @PSBoundParameters }
+
+          $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+          $steppablePipeline.Begin($myInvocation.ExpectingInput, $ExecutionContext)
+      } catch {
+          throw
+      }
+  }
+
+  process
+  {
+      try {
+          $steppablePipeline.Process($_)
+      } catch {
+          throw
+      }
+  }
+
+  end
+  {
+      try {
+          $steppablePipeline.End()
+      } catch {
+          throw
+      }
+  }
+  <#
+    .ForwardHelpTargetName $user_conf_path\utils\With-Env.ps1
+    .ForwardHelpCategory ExternalScript
+  #>
+}
+
