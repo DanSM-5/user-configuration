9e02b54d7db0e5d64acbeaeee286f87a29eef48f
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2023-02-09 20:07:32 -0500

    refactor: Break long command

--- utils/fzf-git.sh
+++ utils/fzf-git.sh
@@ -21,7 +21,9 @@ fgf() {
   git -c color.status=always status --short |
   fzf-down -m --ansi --nth 2..,.. \
     --query "$INITIAL_QUERY" \
-    --preview 'if [ -f {-1} ]; then git diff --color=always -- {-1} | sed 1,4d | bat -p --color=always; bat --color=always {-1}; else ls -aF --color=always {-1}; fi' |
+    --preview 'if [ -f {-1} ]; then git diff --color=always -- {-1} |
+      sed 1,4d |
+      bat -p --color=always; bat --color=always {-1}; else ls -aF --color=always {-1}; fi' |
   cut -c4- | sed 's/.* -> //'
   # --preview '(git diff --color=always -- {-1} | sed 1,4d | bat -p --color=always; cat {-1})' |
 }
