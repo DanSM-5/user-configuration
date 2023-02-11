c246f16bbdff85cfc03fd4926f99a6f7f88d1fa8
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2023-02-10 22:59:29 -0500

    feat: Add highlights.ps1 helper

---
+++ utils/highlight.ps1
@@ -0,0 +1,20 @@
+# Helper for fzf command
+#
+# Highlight is an optional dependency for this script
+
+# Get query to search
+$search_query = $args[0]
+
+# Parse path
+$path_value = $args[1..$args.length]
+$path_value = $path_value.Replace('\\', '/')
+$path_value = "$($PWD.ProviderPath.Replace('\', '/'))/$($path_value.Replace('\\','/'))"
+
+try {
+  highlight -O ansi -l "$path_value" |
+    rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 "$search_query" || rg --ignore-case --pretty --context 10 "$search_query" "$path_value"
+} catch {
+  # Fallback to rg only if highlight is not installed
+  rg --ignore-case --pretty --context 10 "$search_query" "$path_value"
+}
+
