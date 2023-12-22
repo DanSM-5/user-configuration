96577cec03f5380f6c56582f527359186f82d34d
Eduardo Daniel Sanchez <eduardo.daniel.s.m@gmail.com>
2023-12-22 00:18:16 -0500

    feat: Add environment variables

--- .uconfgrc.ps1
+++ .uconfgrc.ps1
@@ -13,6 +13,8 @@ $env:EDITOR = 'nvim'
 $env:user_conf_path = "$user_conf_path"
 $env:user_scripts_path = "$user_scripts_path"
 $env:prj = "$prj"
+$env:WIN_ROOT = "C:\"
+$env:WIH_HOME = "$HOME"
 
 # Set true color support
 $env:COLORTERM = 'truecolor'
