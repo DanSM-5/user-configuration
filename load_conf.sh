4c74f9c7b743f945fdd57b5f46488b70d824164a
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2024-03-18 18:28:47 -0400

    feat: Update load_conf.sh to run with posix sh

--- load_conf.sh
+++ load_conf.sh
@@ -2,7 +2,7 @@
 
 # Identify commands existance
 command_exists () {
-	command -v "$1" &> /dev/null
+	command -v "$1" 2>&1 >/dev/null
 }
 
 # Default values for device detection
@@ -19,32 +19,62 @@ export IS_CMD=false
 export IS_WINSHELL=false # PWSH, GITBASH or CMD
 export IS_NIXONDROID="${IS_NIXONDROID:-false}" # Can only be true if set from home-manager
 
+# Other posible case
+# case "$(bat /proc/version 2> /dev/null)" in
+#   *WSL2*)
+#     echo 'yes'
+#     ;;
+#   [mM]icrosoft)
+#     echo 'WSL'
+#     ;;
+# esac
+
 # Detect if running WSL
-if command_exists /mnt/c/Windows/System32/cmd.exe; then
+# NOTE: No &> redirection in sh
+# if [ -f /mnt/c/Windows/System32/cmd.exe ]; then
+if cat /proc/version 2>&1 | grep '[Mm]icrosoft' 2>&1 >/dev/null; then
   export IS_WINDOWS=true
-  export IS_WSL=true 
-  if [[ $(uname -a) =~ "WSL2" ]]; then
-    export IS_WSL2=true
-  else
-    export IS_WSL1=true
-  fi
+  export IS_WSL=true
+  export IS_LINUX=true
+
+  case "$(uname -a)" in
+    *WSL2*)
+      export IS_WSL2=true
+      ;;
+    *)
+      export IS_WSL1=true
+      ;;
+  esac
+
+  # if [[ $(uname -a) =~ "WSL2" ]]; then
+  #   export IS_WSL2=true
+  # else
+  #   export IS_WSL1=true
+  # fi
+
 elif command_exists termux-setup-storage; then
   export IS_TERMUX=true
-elif command_exists /c/Windows/System32/cmd.exe; then
+  export IS_LINUX=true
+elif [ -f /c/Windows/System32/cmd.exe ]; then
   export IS_WINDOWS=true
   export IS_GITBASH=true
   export IS_WINSHELL=true
+else
+  # Detect System
+  case "$(uname)" in
+    Linux*) export IS_LINUX=true;;
+    Darwin*) export IS_MAC=true;;
+  esac
 fi
 
-# Detect System
-case "$(uname)" in
-  Linux*) export IS_LINUX=true;;
-  Darwin*) export IS_MAC=true;;
-esac
+# If loading from vscode config, we only care about the environment variables above
+if [ "$VSCODE_NVIM" = true ]; then
+  return 0
+fi
 
 # Source User Scripts
 test -f "$HOME/.usr_conf/.uconfgrc" && \. "$HOME/.usr_conf/.uconfgrc"
 test -f "$HOME/.usr_conf/.ualiasgrc" && \. "$HOME/.usr_conf/.ualiasgrc"
 test -f "$HOME/.usr_conf/.uconfrc" && \. "$HOME/.usr_conf/.uconfrc"
 test -f "$HOME/.usr_conf/.ualiasrc" && \. "$HOME/.usr_conf/.ualiasrc"
- 
+
