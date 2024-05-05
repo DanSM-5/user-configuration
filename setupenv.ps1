9967aa9c611c0f28fb0bd0945ca2b83adc2e6b94
daniel <eduardo.daniel.s.m@gmail.com>
2024-05-05 12:35:11 -0400

    feat(setup): Add setup environment scripts

---
+++ setupenv.ps1
@@ -0,0 +1,48 @@
+#!/usr/bin/env pwsh
+
+# Use forward slash '/' in all paths
+# for consistency
+$SHOME = $HOME.Replace('\', '/')
+$mpv_location = if ($IsWindows) { "$SHOME/AppData/Roaming/mpv" } else { "$SHOME/.config/mpv" }
+
+# Required repos
+$repos = @{
+  "$SHOME/user-scripts" = "git@github-personal:DanSM-5/user-scripts";
+  "$SHOME/.SpaceVim.d" = "git@github-personal:DanSM-5/space-vim-config";
+  "$SHOME/.config/vscode-nvim" = "git@github-personal:DanSM-5/vscode-nvim";
+  "$SHOME/omp-theme" = "git@github-personal:DanSM-5/omp-theme";
+}
+
+# Repos that should be clonned within $HOME/user-scripts
+$user_scripts = @{
+  "$SHOME/user-scripts/ff2mpv" = "git@github-personal:DanSM-5/ff2mpv";
+}
+
+# Repos that should be clonned within mpv/scripts
+$mpv_plugins = @{
+  "$mpv_location/scripts/mpv_sponsorblock" = "git@github-personal:DanSM-5/mpv_sponsorblock";
+  "$mpv_location/scripts/mpv-gif-generator" = "git@github-personal:DanSM-5/mpv-gif-generator";
+  "$mpv_location/scripts/mpv-file-browser" = "https://github.com/CogentRedTester/mpv-file-browser";
+}
+
+function try_clone ([string] $location, [string] $repo) {
+
+  # Only clone if dir doesn't exist already
+  if (Test-Path -Path $location -PathType Container -ErrorAction SilentlyContinue) {
+    Write-Output "Repo: $repo already exist in $location"
+  } else {
+    git clone "$repo" "$location"
+  }
+}
+
+function process_list ([HashTable] $array) {
+  foreach ($location in $array.Keys) {
+    $repo = $array[$location]
+    try_clone $location $repo
+  }
+}
+
+process_list $repos
+process_list $user_scripts
+process_list $mpv_plugins
+
---
+++ setupenv.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Detection to handle correct paths per platform
+mpv_location=''
+windows=false
+
+if [[ "$(uname)" =~ MSYS.+|MINGW.+|CYGWIN.+|.+NT.+ ]]; then
+  windows=true
+fi
+
+if [ "$windows" = true ]; then
+  mpv_location="$HOME/AppData/Roaming/mpv"
+else
+  mpv_location="$HOME/.config/mpv"
+fi
+
+# Required repos
+declare -A repos=(
+  ["$HOME/user-scripts"]="git@github-personal:DanSM-5/user-scripts"
+  ["$HOME/.SpaceVim.d"]="git@github-personal:DanSM-5/space-vim-config"
+  ["$HOME/.config/vscode-nvim"]="git@github-personal:DanSM-5/vscode-nvim"
+  ["$HOME/omp-theme"]="git@github-personal:DanSM-5/omp-theme"
+)
+
+# Repos that should be clonned within $HOME/user-scripts
+declare -A user_scripts=(
+  ["$HOME/user-scripts/ff2mpv"]="git@github-personal:DanSM-5/ff2mpv"
+)
+
+# Repos that should be clonned within mpv/scripts
+declare -A mpv_plugins=(
+  ["$mpv_location/scripts/mpv_sponsorblock"]="git@github-personal:DanSM-5/mpv_sponsorblock"
+  ["$mpv_location/scripts/mpv-gif-generator"]="git@github-personal:DanSM-5/mpv-gif-generator"
+  ["$mpv_location/scripts/mpv-file-browser"]="https://github.com/CogentRedTester/mpv-file-browser"
+)
+
+try_clone () {
+  local location="$1"
+  local repo="$2"
+
+  # Only clone if dir doesn't exist already
+  if ! [ -d "$location" ]; then
+    git clone "$repo" "$location"
+  else
+    printf '%s\n' "Repo: $repo already exist in $location"
+  fi
+}
+
+process_list () {
+  local -n array=$1
+  local repo=''
+
+  for location in "${!array[@]}"; do
+    repo="${array["$location"]}"
+    if [ -n "$location" ] && [ -n "$repo" ]; then
+      try_clone "$location" "$repo"
+    fi
+  done
+}
+
+process_list repos
+process_list user_scripts
+process_list mpv_plugins
+
