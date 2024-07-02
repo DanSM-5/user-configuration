2029dab6fb15d24429ef4d7813224b7f3730941a
daniel <eduardo.daniel.s.m@gmail.com>
2024-07-02 11:25:46 -0400

    feat: Update rgfzf script for powershell

--- utils/rgfzf.ps1
+++ utils/rgfzf.ps1
@@ -34,7 +34,7 @@ function FindFzf()
     }
 }
 
-function Get-EditorLaunch() {
+function Get-EditorLaunchOld() {
     param($FileList, $LineNum = 0)
     # HACK to check to see if we're running under Visual Studio Code.
     # If so, reuse Visual Studio Code currently open windows:
@@ -102,7 +102,73 @@ function Get-EditorLaunch() {
     }
 }
 
+function Get-EditorLaunch() {
+  $TEMP_FILE = if ($IsWindows) { '"{+f}"' } else { '{+f}' }
+  $editor = $null
+  $editorOptions = ''
+
+  # HACK to check to see if we're running under Visual Studio Code.
+  # If so, reuse Visual Studio Code currently open windows:
+  if ($null -ne $env:VSCODE_PID) {
+    $editor = 'code'
+    $editorOptions += ' --reuse-window'
+  }
+  else {
+    $editor = if ($ENV:VISUAL) { $ENV:VISUAL }
+      elseif ($ENV:PREFERED_EDITOR) { $ENV:PREFERED_EDITOR }
+      elseif ($ENV:EDITOR) { $ENV:EDITOR }
+      else { nvim }
+    # if ($null -eq $editor) {
+    #     $editor = if (!$IsWindows) { 'vim' } else { 'code' }
+    # }
+  }
+  if (-not [string]::IsNullOrEmpty($env:PSFZF_EDITOR_OPTIONS)) {
+    $editorOptions += ' ' + $env:PSFZF_EDITOR_OPTIONS
+  }
+
+  if ($editor -match '[gn]?vi[m]?') {
+    return @"
+      if (`$env:FZF_SELECT_COUNT -eq 0) {
+        $editor $editorOptions {1} +{2}     # No selection. Open the current line in Vim.
+      } else {
+        $editor $editorOptions +cw -q $TEMP_FILE  # Build quickfix list for the selected items.
+      }
+"@
+  } elseif ($editor -eq 'code' -or $editor -eq 'code-insiders' -or $editor -eq 'codium') {
+    $goto = '"{1}:{2}"'
+    return @"
+      if (`$env:FZF_SELECT_COUNT -eq 0) {
+        `$file = {1};
+        `$line = {2};
+        $editor $editorOptions --goto """`${file}:`${line}"""
+      } else {
+        `$FileList = Get-Content $TEMP_FILE | % {
+          `$files = [System.Collections.Generic.List[string]]::new()
+        } {
+          `$file, `$ignore = `$_ -Split ':';
+          [void]`$files.Add("""`$file""");
+        } { `$files }
+        $editor $editorOptions @FileList
+      }
+"@
+  } else {
+    # TODO: Handle case for nano
+    return @"
+      if (`$env:FZF_SELECT_COUNT -eq 0) {
+        $editor $editorOptions {1}
+      } else {
+        `$FirstFile = Get-Content -TotalCount 1 $TEMP_FILE | % {
+          `$file, `$ignore = `$_ -Split ':';
+          """`$file"""
+        }
+        $editor $editorOptions "`$FirstFile"
+      }
+"@
+  }
+}
+
 function Invoke-PsFzfRipgrep() {
+    # TODO: Make $NoEditor work again
     # this function is adapted from https://github.com/junegunn/fzf/blob/master/ADVANCED.md#switching-between-ripgrep-mode-and-fzf-mode
     param([Parameter(Mandatory)]$SearchString, [switch]$NoEditor)
 
@@ -117,50 +183,74 @@ function Invoke-PsFzfRipgrep() {
     $INITIAL_QUERY = $SearchString
     $results = ''
     $originalFzfDefaultCommand = $env:FZF_DEFAULT_COMMAND
+    # $editor = if ($PREFERED_EDITOR) { $PREFERED_EDITOR } elseif ($EDITOR) { $EDITOR } else { nvim }
 
     try {
         if ($script:IsWindows) {
             $sleepCmd = ''
             $trueCmd = 'cd .'
-            $env:FZF_DEFAULT_COMMAND = "$RG_PREFIX ""$INITIAL_QUERY"""
+            # $env:FZF_DEFAULT_COMMAND = "$RG_PREFIX ""$INITIAL_QUERY"""
+            # $TEMP_FILE = '"{+f}"'
         }
         else {
             $sleepCmd = 'sleep 0.1;'
             $trueCmd = 'true'
-            $env:FZF_DEFAULT_COMMAND = '{0} $(printf %q "{1}")' -f $RG_PREFIX, $INITIAL_QUERY
+            # $env:FZF_DEFAULT_COMMAND = '{0} $(printf %q "{1}")' -f $RG_PREFIX, $INITIAL_QUERY
+            # $TEMP_FILE = '{+f}'
         }
+
+        $RELOAD = "reload:$RG_PREFIX {q} || $trueCmd"
+        $OPENER = Get-EditorLaunch
 
         & $script:FzfLocation --ansi `
-            --color "hl:-1:underline,hl+:-1:underline:reverse" `
-            --disabled --query "$INITIAL_QUERY" `
-            --bind "change:reload:$sleepCmd $RG_PREFIX {q} || $trueCmd" `
-            --bind ("ctrl-f:unbind(change,ctrl-f)+change-prompt" + '( +? fzf> )' + "+enable-search+clear-query+rebind(ctrl-r)") `
-            --bind ("ctrl-r:unbind(ctrl-r)+change-prompt" + '(?? ripgrep> )' + "+disable-search+reload($RG_PREFIX {q} || $trueCmd)+rebind(change,ctrl-f)") `
-            --prompt '?? ripgrep> ' `
-            --delimiter : `
-            --header '? CTRL-R (Ripgrep mode) ? CTRL-F (fzf mode) ?' `
-            --preview 'bat --color=always {1} --highlight-line {2}' `
-            --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' |
-            ForEach-Object { $results += $_ }
+          --header '╱ CTRL-R (Ripgrep mode) ╱ CTRL-F (fzf mode) ╱' `
+          --disabled --ansi --multi `
+          --with-shell "pwsh -NoLogo -NonInteractive -NoProfile -Command" `
+          --bind "0:toggle-preview" `
+          --bind "start:$RELOAD" `
+          --bind "ctrl-l:toggle-preview" `
+          --bind "change:$RELOAD" `
+          --bind "enter:become:$OPENER" `
+          --bind "ctrl-o:execute:$OPENER" `
+          --bind ("ctrl-r:unbind(ctrl-r)+change-prompt" + '(1. ripgrep> )' + "+disable-search+reload($RG_PREFIX {q} || $trueCmd)+rebind(change,ctrl-f)") `
+          --bind ("ctrl-f:unbind(change,ctrl-f)+change-prompt" + '(2. fzf> )' + "+enable-search+clear-query+rebind(ctrl-r)") `
+          --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' `
+          --delimiter : `
+          --preview 'printf "%s" "{+f}"; bat --style=full --color=always --highlight-line {2} {1}' `
+          --preview-window '~4,+{2}+4/3,<80(up)' `
+          --query "$INITIAL_QUERY"
 
-        # Cleanup the FZF_DEFAULT_COMMAND if no longer used
-        $env:FZF_DEFAULT_COMMAND = $originalFzfDefaultCommand
+        # & $script:FzfLocation --ansi `
+        #     --color "hl:-1:underline,hl+:-1:underline:reverse" `
+        #     --disabled --query "$INITIAL_QUERY" `
+        #     --bind "change:reload:$sleepCmd $RG_PREFIX {q} || $trueCmd" `
+        #     --bind ("ctrl-f:unbind(change,ctrl-f)+change-prompt" + '( +? fzf> )' + "+enable-search+clear-query+rebind(ctrl-r)") `
+        #     --bind ("ctrl-r:unbind(ctrl-r)+change-prompt" + '(?? ripgrep> )' + "+disable-search+reload($RG_PREFIX {q} || $trueCmd)+rebind(change,ctrl-f)") `
+        #     --prompt '?? ripgrep> ' `
+        #     --delimiter : `
+        #     --header '? CTRL-R (Ripgrep mode) ? CTRL-F (fzf mode) ?' `
+        #     --preview 'bat --color=always {1} --highlight-line {2}' `
+        #     --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' |
+        #     ForEach-Object { $results += $_ }
 
-        if (-not [string]::IsNullOrEmpty($results)) {
-            # TODO: Upgrade to support multiple selections
-            # foreach ($res in $results) {}
-            $split = $results.Split(':')
-            $fileList = $split[0]
-            $lineNum = $split[1]
-            if ($NoEditor) {
-                Resolve-Path $fileList
-            }
-            else {
-                $cmd = Get-EditorLaunch -FileList $fileList -LineNum $lineNum
-                Write-Host "Executing '$cmd'..."
-                Invoke-Expression -Command $cmd
-            }
-        }
+        # # Cleanup the FZF_DEFAULT_COMMAND if no longer used
+        # $env:FZF_DEFAULT_COMMAND = $originalFzfDefaultCommand
+
+        # if (-not [string]::IsNullOrEmpty($results)) {
+        #     # TODO: Upgrade to support multiple selections
+        #     # foreach ($res in $results) {}
+        #     $split = $results.Split(':')
+        #     $fileList = $split[0]
+        #     $lineNum = $split[1]
+        #     if ($NoEditor) {
+        #         Resolve-Path $fileList
+        #     }
+        #     else {
+        #         $cmd = Get-EditorLaunch -FileList $fileList -LineNum $lineNum
+        #         Write-Host "Executing '$cmd'..."
+        #         Invoke-Expression -Command $cmd
+        #     }
+        # }
     }
     catch {
         Write-Error "Error occurred: $_"
