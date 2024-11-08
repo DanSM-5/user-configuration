add5cff089d635253d9c17e890c3dbcd45d54bb0
daniel <eduardo.daniel.s.m@gmail.com>
2024-11-07 20:37:13 -0500

    fix: User correct name to align with vim cprj

--- .ualiasgrc.ps1
+++ .ualiasgrc.ps1
@@ -461,7 +461,7 @@ function cprj ([Switch] $Raw) {
   $selection = @(
     $directories |
       fzf @options `
-        --history="$env:FZF_HIST_DIR/fzf-change-prj" `
+        --history="$env:FZF_HIST_DIR/cprj" `
         --no-multi `
         --ansi --cycle `
         --info=inline `
