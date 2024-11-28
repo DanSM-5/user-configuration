259a2b6ad061d88f02c3ad3ef9e46f6e4db7f590
daniel <eduardo.daniel.s.m@gmail.com>
2024-11-27 23:40:21 -0500

    feat(fzf-commands): Add utility scripts for fzf commands

---
+++ fzf/alt_c_command.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+
+$fd_exclude_file = "$user_conf_path/fzf/fd_exclude"
+$fd_show_file = "$user_conf_path/fzf/fd_show"
+
+$fd_exclude = Get-Content $fd_exclude_file
+$fd_show = Get-Content $fd_show_file
+
+# Write-Output "Arguments read from fd_exclude_file: $fd_exclude"
+# Write-Output "Arguments read from fd_show_file: $fd_show"
+
+& fd --type directory $fd_show $fd_exclude --color=always @args
+
---
+++ fzf/ctrl_t_command.bat
@@ -0,0 +1,31 @@
+@echo off
+
+REM @For /f "delims=" %%a in ('Type "File.txt"') do ( echo command_name argument --option %%a)
+REM pause
+
+setlocal EnableDelayedExpansion
+
+REM Exclude arguments
+set "fd_exclude="
+set "fd_show="
+
+set "fd_exclude_file=%USERPROFILE%\.usr_conf\fzf\fd_exclude"
+
+for /f "tokens=*" %%a in (%fd_exclude_file%) do (
+  set "fd_exclude=!fd_exclude! %%a"
+)
+
+REM echo Arguments read from fd_exclude_file: %fd_exclude%
+
+set "fd_show_file=%USERPROFILE%\.usr_conf\fzf\fd_show"
+
+for /f "tokens=*" %%a in (%fd_show_file%) do (
+  set "fd_show=!fd_show! %%a"
+)
+
+REM echo Arguments read from fd_show_file: %fd_show%
+
+call fd %fd_show% %fd_exclude% --color=always %*
+
+endlocal
+
